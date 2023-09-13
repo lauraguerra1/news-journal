84ab74045b92f8278686b5c5ad49c234aec9708e
Laura Guerra <l.garciaguerra1@gmail.com>
2023-09-13 13:44:02 -0400

    Switch out description for article content

--- src/components/ArticleDetails/ArticleDetails.tsx
+++ src/components/ArticleDetails/ArticleDetails.tsx
@@ -25,16 +25,13 @@ const ArticleDetails = ({ articles }: ArticleDetailsProps) => {
           <div className='description-page'>
             <article>
               <h2 className='description-page-title'>{article.title}</h2>
-              <p className='description-page-'>{article.description}</p>
               <img className='article-detail-img' src={article.urlToImage} alt={article.title} />
             </article>
             <div className='description-divider'></div>
             <p>By {article.author}</p>
             <p className='description-page-date'>Published on {getStringDate(article.publishedAt)}</p>
-            {/* <div style={{ display: 'flex', justifyContent:'center'}}> */}
-              <a className='article-link' href={article.url} target='_blank'>View the full article here</a>
-            {/* </div> */}
             <div className='description-divider'></div>
+            <p>{article.content.split('[')[0]}<a className='article-link' href={article.url} target='_blank'>Click to Read More</a></p>
           </div>
           : <NotFound />
       }
